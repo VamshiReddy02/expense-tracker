4324cd793c271a6f189076faf1322f6cd64c58b3
Vamshi Reddy <vamshiproject02@gmail.com>
2023-06-26 22:21:01 +0530

    components are added'

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import './App.css';
+import {Header} from './components/Header.js'
+import { Balance } from './components/Balance';
+import { IncomeExpenses } from './components/IncomeExpenses';
+import { TransactionList } from './components/TransactionList';
+import { AddTransaction } from './components/AddTransaction';
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Balance />
+      <IncomeExpenses />
+      <TransactionList />
+      <AddTransaction />
+    </div>
+  );
+}
+
+export default App;

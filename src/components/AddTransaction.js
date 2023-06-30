4061f0c0bec8d0b3991d34f5215170bca88f59c0
Vamshi Reddy <vamshiproject02@gmail.com>
2023-06-30 17:21:22 +0530

    create and implemented add_transaction and delete transaction in GlobalState and AppReducer

--- src/components/AddTransaction.js
+++ src/components/AddTransaction.js
@@ -1,13 +1,30 @@
 import React, {useState} from 'react'
+import {useContext} from 'react';
+import { GlobalContext } from '../context/GlobalState';
+
 
 export const AddTransaction = () => {
     const [text, setText] = useState('');
     const [amount, setAmount] = useState(0);
+    const { addTransaction } = useContext(GlobalContext);
+
+    const onSubmit = e => {
+        e.preventDefault();
+
+        const newTranscation = {
+            id: Math.floor(Math.random() * 100000000),
+            text,
+            amount: +amount
+        }
+
+        addTransaction(newTranscation);
+    }
+
 
   return (
     <>
          <h3>Add new transaction</h3>
-      <form>
+      <form onSubmit={onSubmit}>
         <div className="form-control">
           <label htmlFor="text">Text</label>
           <input type="text" defaultValue={text} onClick={(e) => setText(e.target.value)} placeholder="Enter text..." />
